9911d517f0becd5b15e3e0727f119d23a9e1f5c4
VikasChauhanBD <vikas.chauhan@believersdestination.com>
2026-02-25 11:26:11 +0530

    email

--- src/pages/loginPage/Login.jsx
+++ src/pages/loginPage/Login.jsx
@@ -37,7 +37,7 @@ const Login = () => {
     setLoading(true);
 
     const result = await login({
-      username: formData.email,
+      email: formData.email,
       password: formData.password,
     });
 
